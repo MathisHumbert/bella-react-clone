ab6e280380b1a1d171a2f940ac4dff0f7c89584a
MathisHumbert <mathishumbert71@gmail.com>
2022-11-29 18:51:04 +0100

    added Lenis for smooth scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,32 @@
 import { Routes, Route } from 'react-router-dom';
+import Lenis from '@studio-freight/lenis';
 
 import Home from './pages/Home';
 import Portfolio from './pages/Portfolio';
+import ScrollToTop from './components/ScrollToTop';
+
+const lenis = new Lenis({
+  // @ts-ignore
+  lerp: 0.01,
+  smooth: true,
+  direction: 'vertical',
+});
+
+function raf(time: any) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
 
 export default function App() {
   return (
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/portfolio' element={<Portfolio />} />
-    </Routes>
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/portfolio' element={<Portfolio />} />
+      </Routes>
+    </>
   );
 }
